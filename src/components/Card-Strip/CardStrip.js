0d33f784564191af7c8c2035a3a1d0ec4068de0b
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-22 14:46:14 +0000

    Populating CardStrip with dummy elements

--- src/components/Card-Strip/CardStrip.js
+++ src/components/Card-Strip/CardStrip.js
@@ -1,7 +1,42 @@
 import "./CardStrip.css";
+import Card from "./Card/Card";
 
 const CardStrip = () => {
-	return <div></div>;
+	const loremContent =
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id vestibulum tellus, non dapibus nunc.";
+
+	return (
+		<>
+			{/* Empty tag is a react fragment (temp dummy element, disappears later) */}
+			<h3 className="card-strip-header">Spotify Playlists</h3>
+			<div className="card-strip">
+				<Card
+					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
+					title="Title"
+					content={loremContent}
+					link="#"
+				/>
+				<Card
+					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
+					title="Title"
+					content={loremContent}
+					link="#"
+				/>
+				<Card
+					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
+					title="Title"
+					content={loremContent}
+					link="#"
+				/>
+				<Card
+					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
+					title="Title"
+					content={loremContent}
+					link="#"
+				/>
+			</div>
+		</>
+	);
 };
 
 export default CardStrip;
