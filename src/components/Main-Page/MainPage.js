47956668bfc0c36163b15aaff02e553662dced43
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-22 14:40:53 +0000

    First draft for card strip
    Also centered card component + moved header from MainPage to CardStrip

--- src/components/Main-Page/MainPage.js
+++ src/components/Main-Page/MainPage.js
@@ -1,11 +1,7 @@
 import "./MainPage.css";
 
 const MainPage = () => {
-	return (
-		<div className="main-page">
-			<h3 className="main-page-header">Spotify Playlists</h3>
-		</div>
-	);
+	return <div className="main-page"></div>;
 };
 
 export default MainPage;
