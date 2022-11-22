8259bdadd10f0c6eccdfe969d436501e7730af00
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-22 19:33:31 +0000

    Added dummy content to MainPage
    Also made sidebar and navbar sticky

--- src/components/Main-Page/MainPage.js
+++ src/components/Main-Page/MainPage.js
@@ -1,7 +1,15 @@
+import CardStrip from "../Card-Strip/CardStrip";
 import "./MainPage.css";
 
 const MainPage = () => {
-	return <div className="main-page"></div>;
+	return (
+		<div className="main-page">
+			<CardStrip header="Spotify Playlists" numberOfCards={4} />
+			<CardStrip header="Focus" numberOfCards={4} />
+			<CardStrip header="Mood" numberOfCards={4} />
+			<CardStrip header="Popular new releases" numberOfCards={4} />
+		</div>
+	);
 };
 
 export default MainPage;
