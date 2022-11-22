b49b5d8e876f751bc82c22cade58c802eb6e59e0
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-22 11:25:30 +0000

    First draft of card element

---
+++ src/components/Card-Strip/Card/Card.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./Card.css";
+import PlayButton from "../../../assets/images/play-button.png";
+
+const Card = ({ imgSrc, title, content, link }) => {
+	const [playButtonClasses, setPlayButtonClasses] = useState(
+		"play-button-container hidden"
+	);
+
+	const handleMouseEnter = () => {
+		setPlayButtonClasses("play-button-container");
+	};
+
+	const handleMouseLeave = () => {
+		setPlayButtonClasses("play-button-container hidden");
+	};
+
+	return (
+		<div className="card">
+			<a
+				href={link}
+				className="card-container"
+				onMouseEnter={handleMouseEnter}
+				onMouseLeave={handleMouseLeave}
+			>
+				<img src={imgSrc} className="card-image" alt="An artist" />
+				<div className={playButtonClasses}>
+					<img
+						src={PlayButton}
+						alt="Play button"
+						className="card-play-button"
+					/>
+				</div>
+				<h3 className="card-title">{title}</h3>
+				<p className="card-content">{content}</p>
+			</a>
+		</div>
+	);
+};
+
+export default Card;
