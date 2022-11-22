975baec5ea313a34d730f644d82fed9c8c6b0123
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-11-22 15:03:26 +0000

    Dynamic headers + number fo cards in CardStrip using props

--- src/components/Card-Strip/CardStrip.js
+++ src/components/Card-Strip/CardStrip.js
@@ -1,39 +1,34 @@
 import "./CardStrip.css";
 import Card from "./Card/Card";
 
-const CardStrip = () => {
+const CardStrip = ({ header, numberOfCards }) => {
 	const loremContent =
 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id vestibulum tellus, non dapibus nunc.";
+	const imgSrc =
+		"https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48";
+
+	const prepCards = (num) => {
+		let arr = [];
+		for (let i = 0; i < num; i++) {
+			arr.push(
+				<Card
+					key={i}
+					imgSrc={imgSrc}
+					title="Title"
+					content={loremContent}
+					link="#"
+				/>
+			);
+		}
+		return arr;
+	};
 
 	return (
 		<>
 			{/* Empty tag is a react fragment (temp dummy element, disappears later) */}
-			<h3 className="card-strip-header">Spotify Playlists</h3>
+			<h3 className="card-strip-header">{header}</h3>
 			<div className="card-strip">
-				<Card
-					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
-					title="Title"
-					content={loremContent}
-					link="#"
-				/>
-				<Card
-					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
-					title="Title"
-					content={loremContent}
-					link="#"
-				/>
-				<Card
-					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
-					title="Title"
-					content={loremContent}
-					link="#"
-				/>
-				<Card
-					imgSrc="https://i.scdn.co/image/ab67706f00000002b9c1ff6dac384a87747d5f48"
-					title="Title"
-					content={loremContent}
-					link="#"
-				/>
+				{prepCards(numberOfCards).map((value) => value)}
 			</div>
 		</>
 	);
